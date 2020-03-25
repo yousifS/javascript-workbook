856b009e50c4de17bfba24e45a14dee59991b19e
Yousif Seedhom <yousif.seedhom@gmail.com>
2020-03-24 20:22:36 -0500

    Initial solution to the datatypes assignment

---
+++ 01week/datatypes.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//Write a JavaScript program to display the current day and time.
+let now = new Date();
+console.log("the current date and time", now);
+
+//Write a JavaScript program to convert a number to a string.
+const theNumber = 5;
+const theString = theNumber.toString();
+
+console.log("The type of theString variable is", (typeof theString));
+console.log("The type of theNumber variable is", (typeof theNumber));
+console.log("The string is",theString);
+console.log("the number is",theNumber);
+
+// Write a JavaScript program to convert a string to the number.
+const theOtherString = "11";
+const theOtherNumber = parseInt(theOtherString, 10);
+console.log("The otherString type is", (typeof theOtherString));
+console.log("The theOtherNumber type is", (typeof theOtherNumber));
+
+
